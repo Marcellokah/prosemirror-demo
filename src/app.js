045332fec4e00d66a6f62502c7775c4bc5536239
Marcellokah <aalma47@gmail.com>
2024-04-17 09:56:26 +0200

    Fix link, remove comment

--- src/app.js
+++ src/app.js
@@ -5,8 +5,6 @@ import { schema } from "prosemirror-schema-basic"
 import { addListNodes } from "prosemirror-schema-list"
 import { exampleSetup } from "prosemirror-example-setup"
 
-// Mix the nodes from prosemirror-schema-list into the basic schema to
-// create a schema with list support.
 const mySchema = new Schema({
     nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
     marks: schema.spec.marks
